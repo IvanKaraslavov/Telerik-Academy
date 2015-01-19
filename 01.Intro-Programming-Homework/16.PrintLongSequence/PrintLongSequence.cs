cae9471e72d6fe485278033e33ed9129d93083c9
IvanKaraslavov <ivan.karaslavov@hotmail.com>
2015-01-19 21:40:27 +0200

    Update PrintLongSequence.cs

--- 01.Intro-Programming-Homework/16.PrintLongSequence/PrintLongSequence.cs
+++ 01.Intro-Programming-Homework/16.PrintLongSequence/PrintLongSequence.cs
@@ -1,23 +1,21 @@
-﻿using System;
+using System;
 
        //Write a program that prints the first 1000 members of the sequence: 2, -3, 4, -5, 6, -7, …
 
-    class PrintLongSequence
-    {
-        static void Main()
-        {
-            for (int i = 2; i <= 1001; i++)
-            {
-                if (i % 2 == 0)
-                {
-                    Console.WriteLine(i);
-                }
-                else
-                {
-                    Console.WriteLine(i * (-1));
-                }
-                
-            }
-        }
-    }
-
+class LongSequence
+{
+       static void Main()
+       {
+              for (int i = 2; i < 1001; i++)
+              {
+                  if (i % 2 !=0)
+                 {
+                     Console.Write(-i+" ");
+                 }
+                  else
+                 {
+                    Console.Write(i + " ");
+                 }
+              }
+       }
+}
