1059429adb6936df6ddd658b5a15c2577b966ae7
IvanKaraslavov <ivan.karaslavov@hotmail.com>
2015-01-23 20:44:02 +0200

    add

---
+++ 06.Loops-Homework/MatrixOfNumbers/MatrixOfNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+    class MatrixOfNumbers
+    {
+        static void Main()
+        {
+                //Write a program that reads from the console a positive integer number n (1 = n = 20) and prints a matrix like
+                //in the examples below. Use two nested loops.
+
+            Console.WriteLine("Please enter your number: ");
+            int n = int.Parse(Console.ReadLine());
+            int howMany = 0;
+            int result = n + howMany;
+            for (int row = 1; row <= n; row++)
+            {
+                
+                Console.WriteLine();
+                for (int i = row; i <= n + howMany ; i++)
+                {
+                     Console.Write("{0} ", i);      
+                }
+                howMany++;
+            }
+        }
+    }
+
---
+++ 06.Loops-Homework/OddAndEvenProduct/OddAndEvenProduct.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class OddAndEvenProduct
+{
+    static void Main()
+    {
+            //You are given n integers (given in a single line, separated by a space).
+            //Write a program that checks whether the product of the odd elements is equal to the product of the even elements.
+            //Elements are counted from 1 to n, so the first element is odd, the second is even, etc.
+
+        int even = 1;
+        int odd = 1;
+        Console.WriteLine("Enter your numbers in a single line, separated by a space:");
+        string readNumbers = Console.ReadLine();
+        string[] numbers = readNumbers.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            int number = int.Parse(numbers[i]);
+            if (i % 2 == 0)
+            {
+                even *= number;
+            }
+            else
+            {
+                odd *= number;
+            }
+
+        }
+        if (even == odd)
+        {
+            Console.WriteLine("Result: yes");
+            Console.WriteLine("Product: {0}", even);
+        }
+        else
+        {
+            Console.WriteLine("Result: no");
+            Console.WriteLine("Odd_Product: {0}", odd);
+            Console.WriteLine("Even_Product: {0}", even
+);
+        }
+    }
+}
+
+
